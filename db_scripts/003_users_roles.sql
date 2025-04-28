64e320afef73257e84c5e72ac8b219d65692afef
unknown <erika.programmer@gmail.com>
2025-04-28 17:20:18 -0500

    Adding FK to users_roles

--- db_scripts/003_users_roles.sql
+++ db_scripts/003_users_roles.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS users_roles (
 );
 
 ALTER TABLE users_roles ADD CONSTRAINT UK_user_role UNIQUE (user_id, role_id);
+
+ALTER TABLE users_roles
+ADD CONSTRAINT FK_users_users_roles FOREIGN KEY (user_id)
+REFERENCES users(id);
+
+ALTER TABLE users_roles
+ADD CONSTRAINT FK_roles_users_roles FOREIGN KEY (role_id)
+REFERENCES roles(id);
