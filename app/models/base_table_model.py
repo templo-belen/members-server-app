86e23d3d1a00a589815e34609ab25da24c3e3c1d
ErisoHV <erika.programmer@gmail.com>
2025-05-09 19:31:52 -0500

    Adding missing columns, fixing relations with other tables

--- app/models/base_table_model.py
+++ app/models/base_table_model.py
@@ -4,7 +4,7 @@ from app.database import Base
 
 class BaseTableModel(Base):
     __abstract__ = True
-    id = Column(Integer, primary_key=True, index=True)
+
     created_at = Column(TIMESTAMP)
     updated_at = Column(TIMESTAMP)
     created_by = Column(String(50))
---
+++ db_scripts/008_preaching_point.sql
@@ -0,0 +1,44 @@
+-- DEW tab from members data
+
+CREATE TABLE preaching_point (
+    id SERIAL PRIMARY KEY,
+
+    name VARCHAR(30) NOT NULL UNIQUE,
+
+    status                   varchar(1)   NOT NULL DEFAULT 'A', -- A=Active, I=Inactive/deleted
+    created_at               TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by               VARCHAR(50)  NOT NULL DEFAULT 'SYS',
+    updated_at               TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_by               VARCHAR(50)  NOT NULL DEFAULT 'SYS'
+
+);
+
+INSERT INTO preaching_point (name) VALUES
+('CHIGORODÓ'),
+('CIUDAD BOLÍVAR'),
+('CONCORDIA'),
+('DON MATÍAS'),
+('ENTRERRIOS'),
+('GACHANCIPÁ'),
+('GIRARDOTA'),
+('HISPANIA'),
+('ISTMINA'),
+('JARDÍN'),
+('LA PINTADA'),
+('SABANETA'),
+('SALGAR'),
+('SAN JOSÉ DEL NUS'),
+('SAN PEDRO'),
+('SAN VICENTE FERRER'),
+('SUESCA'),
+('TÁMESIS'),
+('TEMPLO BELÉN'),
+('TB13'),
+('TB SUR')
+ON CONFLICT (name) DO NOTHING;
+;
+
+-- Relation in members table
+ALTER TABLE members
+ADD CONSTRAINT FK_members_preaching_point FOREIGN KEY (preaching_point_id)
+REFERENCES preaching_point(id);
