3a629a8b484cf4a583baf00f95f9035c472cffb1
ErisoHV <erika.programmer@gmail.com>
2025-05-22 19:53:11 -0500

    Fixing names in DEW model and organizing scripts

--- app/models/member_dew.py
+++ app/models/member_dew.py
@@ -11,8 +11,8 @@ class MembersDEWResponse(BaseModel):
     worker_1: Optional[str] = Field(description="Worker 1", alias="worker1")
     worker_2: Optional[str] = Field(description="Worker 2", alias="worker2")
     is_sharing_testimony: Optional[bool] = Field(description="Is sharing testimony", alias="isSharingTestimony")
-    is_publishing_testimony: Optional[bool] = Field(description="Is publishing testimony", alias="isPubilshingTestimony")
-    is_publishing_testimony_name: Optional[bool] = Field(description="Is publishing testimony name", alias="isPubilshingTestimonyName")
+    is_publishing_testimony: Optional[bool] = Field(description="Is publishing testimony", alias="isPublishingTestimony")
+    is_publishing_testimony_name: Optional[bool] = Field(description="Is publishing testimony name", alias="isPublishingTestimonyName")
     is_agreed_share_testimony: Optional[bool] = Field(description="Is agreed share", alias="isAgreedShareTestimony")
 
     model_config = ConfigDict(
