f2c198858ce5ed018b2e54340ab9077e2ffdd765
ErisoHV <erika.programmer@gmail.com>
2025-06-25 13:13:23 -0500

    POST service for family-data and fixing environment variables and flyway

--- app/services/member_family_data.py
+++ app/services/member_family_data.py
@@ -1,8 +1,17 @@
 from sqlalchemy.orm import Session
 
-from app.database import MemberFamilyData
+from app.database import Member, MemberFamilyData
 from app.database.member_children import MemberChildren
-from app.models import FamilyDataResponse, MemberChildrenDataResponse, MemberFamilyDataResponse
+from app.middlewares import current_user_ctx
+from app.models import (
+    CreateFamilyDataRequest,
+    CreateMemberChildrenDataRequest,
+    CreateMemberFamilyDataRequest,
+    FamilyDataResponse,
+    MemberChildrenDataResponse,
+    MemberFamilyDataResponse,
+)
+from app.services import ConflictException, NotFoundException
 
 
 class MembersFamilyDataService:
@@ -28,3 +37,52 @@ class MembersFamilyDataService:
         return MemberFamilyDataResponse(
             familyData=member_family_data_response, childrenDataList=member_children_response
         )
+
+    def create_member_family_data(
+        self, member_id: int, new_member_family_data: CreateMemberFamilyDataRequest, db: Session
+    ) -> MemberFamilyDataResponse:
+        member = db.query(Member).filter(Member.id == member_id).first()
+        if not member:
+            raise NotFoundException(f"El miembro con id {member_id} no existe.")
+
+        self.validate_member_family_data(member_id, db)
+
+        db_member_family_data = None
+        if new_member_family_data.family_data:
+            db_member_family_data = self.add_family_data(member_id, new_member_family_data.family_data, db)
+
+        db_member_children = []
+        if new_member_family_data.children_data_list and len(new_member_family_data.children_data_list) > 0:
+            db_member_children = self.add_children_data(member_id, new_member_family_data.children_data_list, db)
+
+        member.updated_by = current_user_ctx.get().username
+        db.commit()
+        db.refresh(member)
+
+        return MemberFamilyDataResponse(
+            family_data=(FamilyDataResponse.model_validate(db_member_family_data) if db_member_family_data else None),
+            children_data_list=[MemberChildrenDataResponse.model_validate(child) for child in db_member_children],
+        )
+
+    def validate_member_family_data(self, member_id: int, db: Session):
+        member_family_data = self.find_by_member_id(member_id, db)
+        if member_family_data:
+            raise ConflictException(f"El miembro con id {member_id} ya tiene datos familiares")
+
+    def add_family_data(self, member_id: int, family_data: CreateFamilyDataRequest, db: Session):
+        db_member_family_data = MemberFamilyData(**family_data.model_dump(exclude_unset=True))
+        db_member_family_data.member_id = member_id
+        db.add(db_member_family_data)
+        db.flush()
+        return db_member_family_data
+
+    def add_children_data(self, member_id: int, children_data_list: [CreateMemberChildrenDataRequest], db: Session):
+        db_member_children = []
+        for child in children_data_list:
+            db_child_data = MemberChildren(**child.model_dump(exclude_unset=True))
+            db_child_data.member_id = member_id
+            db_member_children.append(db_child_data)
+
+        db.add_all(db_member_children)
+        db.flush()
+        return db_member_children
